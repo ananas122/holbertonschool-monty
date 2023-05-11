5987d333cd18cf3b93463d44987074fce9a091d6
ananas122 <melle.noraagmail.com>
2023-05-11 15:16:58 -0700

    Create function to print values of the stacks

---
+++ _pop.c
@@ -0,0 +1,23 @@
+#include "monty.h"
+
+/**
+ * _pop - desempiler the top element of the stack.
+ * @stack: Double pointer to the head of the stack.
+ * @line_number: Line number of the opcode in the Monty file.
+ */
+void _pop(stack_t **stack, unsigned int line_number)
+{
+    stack_t *tmp = NULL;
+
+    if (*stack == NULL)
+    {
+        fprintf(stderr, "L%d: can't pop an empty stack\n", line_number);
+        exit(EXIT_FAILURE);
+    }
+
+    tmp = *stack;
+    *stack = (*stack)->next;
+    free(tmp);
+}
+
+
