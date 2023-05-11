931f9a732adc8fbd04d5c5e6e84694e9904b995c
ananas122 <melle.noraagmail.com>
2023-05-11 15:20:09 -0700

    Create file nothing

---
+++ _nop.c
@@ -0,0 +1,14 @@
+#include "monty.h"
+
+/**
+ * _nop - Ne rien faire
+ * @stack: Double pointeur sur la tête de la pile
+ * @line_number: Numéro de la ligne en cours d'exécution
+ */
+
+void _nop(stack_t **stack, unsigned int line_number)
+{
+    (void)stack;
+    (void)line_number;
+}
+
