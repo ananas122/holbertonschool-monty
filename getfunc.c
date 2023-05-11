ad0edcb9386af233c09d780cc95337b358b2b8c8
xdJidx <bernardjerome59@gmail.com>
2023-05-11 14:32:03 -0700

    Create instruction for getfunc

--- getfunc.c
+++ getfunc.c
@@ -1,27 +1,29 @@
 #include "monty.h"
 
 /**
- * get_op_func - selects the correct function to perform the
+ * getfunc - selects the correct function to perform the
  * operation asked by the opcode in the script file
  * @opcode: the opcode to be evaluated
  * Return: pointer to the function that corresponds to the opcode
  */
-void (*get_op_func(char *opcode))(stack_t **stack, unsigned int line_number)
+void (*getfunc(char *opcode))(stack_t **stack, unsigned int line_number)
 {
-	int i = 0;
-	instruction_t op_struct[] = {
-		{"push", op_push},
-		{"pall", op_pall},
+        int i = 0;
+        instruction_t op_struct[] = {
+                {"pall", _pall},
+                {"pint", NULL},
+		{"pop", NULL},
+		{"swap", NULL},
+		{"add", NULL},
+		{"nop", NULL},
 		{NULL, NULL}
-  	};
+        };
 
-	/** Loop through array and compare token to opcode */
-	for (int i = 0; op_struct[i].op_func != NULL; i++)
-	{
-		if (strcmp(opcode, op_struct[i].op_func) == 0)
-			return (op_struct[i].f);
-	}
-
+        /** Loop through array and compare token to opcode */
+        for (; op_struct[i].opcode != NULL; i++)
+        {
+                if (strcmp(opcode, op_struct[i].opcode) == 0)
+                        return (op_struct[i].f);
+        }
 	return (NULL);
 }
-
