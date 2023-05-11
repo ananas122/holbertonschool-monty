0b0599bf6478abe24ebdf2ddd1ddde5c2c9d8f3b
xdJidx <bernardjerome59@gmail.com>
2023-05-11 14:15:07 -0700

    Correction of main function to open, read and token file

--- monty.c
+++ monty.c
@@ -8,21 +8,73 @@
 
 int main(int ac, char **av)
 {
-	char **tokenfile;
-	int i;
+	int line = 1;
+	ssize_t openFile, readFile;
+	char *buffer, *token;	
+	int isPush = 0;
+	stack_t *h = NULL;
 
 	if (ac != 2)
 	{
 		dprintf(2, "Usage: %s file\n", av[0]);
-		exit(EXIT_FAILURE);
+		exit (EXIT_FAILURE);
  	}
-
-	tokenfile = readTextFile(av[1], 1024);
 	
-	for (i = 0; tokenfile[i][0]; i++)
+	openFile = open(av[1], O_RDONLY);
+	if (openFile == -1)
 	{
-		get_op_func(tokenfile[i][0]);
+		printf("ERROR");
+		exit (EXIT_FAILURE);
 	}
 
-    return (0);
+	buffer = malloc(sizeof(char) * 10000);
+	if (!buffer)
+		return (0);
+
+	readFile = read(openFile, buffer, 10000);
+	if (readFile == -1)
+	{
+		free(buffer);
+		close(openFile);
+		exit (EXIT_FAILURE);
+	}	
+
+	token = strtok(buffer, DELIMS);
+
+	while (token != NULL)
+	{
+		if (isPush == 1)
+		{
+			_push(&h, line, token);
+			isPush = 0;
+			token = strtok(NULL, DELIMS);
+			line++;
+			continue;
+		}
+		else if (strcmp(token, "push") == 0)
+		{
+			isPush = 1;
+			token = strtok(NULL, DELIMS);
+			continue;
+		}
+		else 
+		{
+			if (getfunc(token) != 0)
+				getfunc(token)(&h, line);
+			else
+			{
+				_freeList (&h);
+				printf("L%d: unknown instruction %s\n", line, token);
+				exit (EXIT_FAILURE);
+			}			
+		}
+		
+		line++;
+		token = strtok(NULL, DELIMS);	
+	}
+		
+	_freeList(&h);
+	free(buffer);
+	close(openFile);
+	return (0);
 }
