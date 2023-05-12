d7324651f10422d2ce3bf47ed40f82b1c779b883
ananas122 <melle.noraagmail.com>
2023-05-12 05:02:23 -0700

    Add comments for to descript the steps

--- freeList.c
+++ freeList.c
@@ -3,17 +3,20 @@
 /**
  * _freeList - Frees a list_t list.
  * @head: A pointer to the list_t list.
+ *
  */
 
 void _freeList(stack_t **head)
 {
+	 /* Vérifier si la tête est NULL */
 	if (!head)
 		return;
-
+	/* Parcourir la liste chainée et libérer chaque noeud */
 	while (*head && (*head)->next)
 	{
 		*head = (*head)->next;
 		free((*head)->prev);
 	}
+	/* Libérer la mémoire occupée par la tête de la liste */
 	free(*head);
 }
