63ef88df74fcc9276f70e45670a53b9440ed40e8
ananas122 <melle.noraagmail.com>
2023-05-12 04:49:35 -0700

    To add comment

--- _pop.c
+++ _pop.c
@@ -10,13 +10,17 @@ void _pop(stack_t **stack, unsigned int line_number)
 {
 	stack_t *tmp = NULL;
 
+	/* si la pile est vide*/
 	if (*stack == NULL)
 	{
 		fprintf(stderr, "L%d: can't pop an empty stack\n", line_number);
 		exit(EXIT_FAILURE);
 	}
 
+	/* Stocke le sommet dans une variable temporaire */
 	tmp = *stack;
+	/* Met à jour le sommet en le remplaçant par le suivant */
 	*stack = (*stack)->next;
+	/* Libère la mémoire allouée pour l'ancien sommet*/
 	free(tmp);
 }
