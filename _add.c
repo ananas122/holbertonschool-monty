8465ff0ef55f5ca0baa961d148be2da25b956815
xdJidx <bernardjerome59@gmail.com>
2023-05-12 00:45:58 -0700

    Correction add.c trailing space

--- _add.c
+++ _add.c
@@ -1,8 +1,11 @@
 #include "monty.h"
 
-/** _add - Ajout des 2 premiers elements de la pile
+/**
+ * _add - Ajout des 2 premiers elements de la pile
+ *
  * @stack: Double pointeur sur la tête de la pile
  * @line_number: Numéro de la ligne en cours d'exécution
+ *
  */
 
 void _add(stack_t **stack, unsigned int line_number)
@@ -12,11 +15,10 @@ void _add(stack_t **stack, unsigned int line_number)
 	/* Vérifier si la P contient 2 éléments */
 	if (*stack == NULL || (*stack)->next == NULL)
 	{
-                fprintf(stderr, "L%d: can't add, stack too short\n", line_number);
-                exit(EXIT_FAILURE);
-        }
+		fprintf(stderr, "L%d: can't add, stack too short\n", line_number);
+		exit(EXIT_FAILURE);
+	}
 
-	
 	/* Obtenir les valeurs des 2 1er éléments*/
 	a = (*stack)->n;
 	b = (*stack)->next->n;
@@ -25,5 +27,3 @@ void _add(stack_t **stack, unsigned int line_number)
 	_pop(stack, line_number);
 	(*stack)->n = result;
 }
-
-
