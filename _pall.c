11295d516d6fce4af150e60365bcbc14ed99d774
xdJidx <bernardjerome59@gmail.com>
2023-05-12 00:46:37 -0700

    Correction pall.c trailing space

--- _pall.c
+++ _pall.c
@@ -1,7 +1,7 @@
 #include "monty.h"
 
 /**
- * op_pall - prints all the values on the stack
+ * _pall - prints all the values on the stack
  * starting from the top of the stack
  * @stack: double pointer to the beginning of the stack
  * @line_number: line number being interpreted from script file
@@ -13,8 +13,8 @@ void _pall(stack_t **stack, unsigned int __attribute__((unused)) line_number)
 
 	/* Parcourir la pile tant que le ptr tmp n'est pas NULL*/
 	while (tmp)
-  	{
-    		printf("%d\n", tmp->n); 
-    		tmp = tmp->next;
-  	}
+	{
+		printf("%d\n", tmp->n);
+		tmp = tmp->next;
+	}
 }
