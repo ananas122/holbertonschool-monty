55a6a425eeedf045c7f32a237ab580d4583703eb
xdJidx <bernardjerome59@gmail.com>
2023-05-12 00:47:15 -0700

    Correction pop.c trailing space

--- _pop.c
+++ _pop.c
@@ -5,19 +5,18 @@
  * @stack: Double pointer to the head of the stack.
  * @line_number: Line number of the opcode in the Monty file.
  */
+
 void _pop(stack_t **stack, unsigned int line_number)
 {
-    stack_t *tmp = NULL;
+	stack_t *tmp = NULL;
 
-    if (*stack == NULL)
-    {
-        fprintf(stderr, "L%d: can't pop an empty stack\n", line_number);
-        exit(EXIT_FAILURE);
-    }
+	if (*stack == NULL)
+	{
+		fprintf(stderr, "L%d: can't pop an empty stack\n", line_number);
+		exit(EXIT_FAILURE);
+	}
 
-    tmp = *stack;
-    *stack = (*stack)->next;
-    free(tmp);
+	tmp = *stack;
+	*stack = (*stack)->next;
+	free(tmp);
 }
-
-
