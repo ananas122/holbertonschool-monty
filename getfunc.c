aba216a2bac97ee47a3f4eb7fcc8e8b4ec75d828
xdJidx <bernardjerome59@gmail.com>
2023-05-12 00:49:09 -0700

    Correction getfunc.c trailing space

--- getfunc.c
+++ getfunc.c
@@ -6,24 +6,25 @@
  * @opcode: the opcode to be evaluated
  * Return: pointer to the function that corresponds to the opcode
  */
+
 void (*getfunc(char *opcode))(stack_t **stack, unsigned int line_number)
 {
-        int i = 0;
-        instruction_t op_struct[] = {
-                {"pall", _pall},
-                {"pint", NULL},
+	int i = 0;
+	instruction_t op_struct[] = {
+		{"pall", _pall},
+		{"pint", NULL},
 		{"pop", NULL},
 		{"swap", NULL},
 		{"add", NULL},
 		{"nop", NULL},
 		{NULL, NULL}
-        };
+	};
 
-        /** Loop through array and compare token to opcode */
-        for (; op_struct[i].opcode != NULL; i++)
-        {
-                if (strcmp(opcode, op_struct[i].opcode) == 0)
-                        return (op_struct[i].f);
-        }
+	/** Loop through array and compare token to opcode */
+	for (; op_struct[i].opcode != NULL; i++)
+	{
+		if (strcmp(opcode, op_struct[i].opcode) == 0)
+			return (op_struct[i].f);
+	}
 	return (NULL);
 }
